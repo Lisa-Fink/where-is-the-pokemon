822bc8028c6fb421dc5488c49133dcdc6a30fc64
Lisa Fink <lisadfink@gmail.com>
2022-07-06 00:20:23 -0400

    Created photo component with image

---
+++ src/Components/Photo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import photo from '../img1.jpg';
+
+function Photo() {
+  return (
+    <div>
+      <img
+        src={photo}
+        alt="big pokemon pic"
+        className="bigpokemon"
+        onClick={(e) =>
+          console.log(
+            e.pageX - e.target.offsetLeft,
+            e.pageY - e.target.offsetTop
+          )
+        }
+      />
+    </div>
+  );
+}
+
+export default Photo;
