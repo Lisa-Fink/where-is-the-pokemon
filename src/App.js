2b9d8c5ad3591c41de7278b7387f9c9a1f738fea
Lisa Fink <lisadfink@gmail.com>
2022-07-05 23:48:57 -0400

    Created global state for timer, char, and score

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import './App.css';
+import Heading from './Components/Heading';
+import Timer from './Components/Timer';
+
+function App() {
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [timerStatus, setTimerStatus] = useState(false);
+
+  const startTimer = () => setTimerStatus(true);
+  const stopTimer = () => setTimerStatus(false);
+  const resetTimer = () => setSeconds(0);
+
+  const [score, setScore] = useState(0);
+  const [chars, setChars] = useState([]);
+
+  return (
+    <div className="App">
+      <Heading score={score} chars={chars} />
+      <Timer
+        seconds={seconds}
+        setSeconds={setSeconds}
+        minutes={minutes}
+        setMinutes={setMinutes}
+        timerStatus={timerStatus}
+        startTimer={startTimer}
+      />
+    </div>
+  );
+}
+
+export default App;
