dc851b2d7f6a7d678869cb42676dd7e5e7680477
Lisa Fink <lisadfink@gmail.com>
2022-07-09 12:57:23 -0400

    Switched geodude for charmander

--- src/Components/charArr.js
+++ src/Components/charArr.js
@@ -1,7 +1,7 @@
 // pokemon thumbnails
 import gengar from '../pokemon/gengar.webp';
 import bulb from '../pokemon/bulbasaur.webp';
-import geo from '../pokemon/geo.webp';
+import charmander from '../pokemon/charmander.webp';
 import horsea from '../pokemon/horsea.webp';
 import ivy from '../pokemon/ivysaur.webp';
 import jiggly from '../pokemon/jiggly.webp';
@@ -10,7 +10,7 @@ import mime from '../pokemon/mime.webp';
 const charsArr = [
   { name: 'Gengar', img: gengar },
   { name: 'Bulbasaur', img: bulb },
-  { name: 'Geodude', img: geo },
+  { name: 'Charmander', img: charmander },
   { name: 'Horsea', img: horsea },
   { name: 'Ivysaur', img: ivy },
   { name: 'Jigglypuff', img: jiggly },
