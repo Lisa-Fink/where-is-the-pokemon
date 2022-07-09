60f03d0094862ca0d75d2c7730e13107ee9d408d
Lisa Fink <lisadfink@gmail.com>
2022-07-09 12:59:08 -0400

    Changed check location to search in a box

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -29,16 +29,23 @@ function Photo({ charsClicked, setCharsClicked, gameStatus, newGame, score }) {
   const checkLocations = async (e) => {
     const x = e.pageX - e.target.offsetLeft;
     const y = e.pageY - e.target.offsetTop;
+    console.log('x:', x, 'y:', y);
     const locations = await getLocations();
-    const checkX = locations.filter((obj) => Math.abs(obj.x - x) <= 20);
-    if (checkX.length) {
-      const checkY = checkX.filter((obj) => Math.abs(obj.y - y) <= 20);
-      if (checkY.length) {
-        // need to do something if more than 1 is in range
-        return checkY[0].name;
+    for (const obj of locations) {
+      // obj.x and obj.y are arrays containing 4 values for x and y outlining a
+      // box around the character.
+      // checks if clicked x is right of the left line, and left of the right line
+      if ((x > obj.x[0]) | (x > obj.x[2]) && (x < obj.x[1]) | (x < obj.x[3])) {
+        //checks if clicked y is below the top line and above the bottom line
+        if (
+          (y > obj.y[0]) | (y > obj.y[1]) &&
+          (y < obj.y[2]) | (y < obj.y[3])
+        ) {
+          return obj.name;
+        }
       }
     }
-
+    // need to do something if more than 1 is in range
     return false;
   };
 
