614da41c8240b1d905c27e66ab9f7b34adbaa4c8
Lisa Fink <lisadfink@gmail.com>
2022-07-08 22:06:51 -0400

    Added firebase config info

---
+++ src/Components/firebaseConfig.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAM0uvh7roYKc6fafQYd48IZDkFbSZAuBU',
+  authDomain: 'waldo-46ee8.firebaseapp.com',
+  projectId: 'waldo-46ee8',
+  storageBucket: 'waldo-46ee8.appspot.com',
+  messagingSenderId: '632531266490',
+  appId: '1:632531266490:web:553854bf9b3612f0254b83',
+};
+
+export default firebaseConfig;
