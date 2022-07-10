a061cc0a1c9813db28aed0f88faef849c96300cb
Lisa Fink <lisadfink@gmail.com>
2022-07-09 20:44:16 -0400

    Moved converting to minutes to use effect

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,18 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
-const Timer = ({
-  seconds,
-  setSeconds,
-  minutes,
-  setMinutes,
-  timerStatus,
-  startTimer,
-}) => {
-  const timeConverter = () => {
+const Timer = ({ seconds, setSeconds, minutes, setMinutes, timerStatus }) => {
+  useEffect(() => {
     if (seconds === 60) {
       setMinutes(minutes + 1);
       setSeconds(0);
     }
+  }, [seconds, minutes]);
+
+  const timeConverter = () => {
     return `${minutes < 10 ? '0' + minutes : minutes}:${
       seconds < 10 ? '0' + seconds : seconds
     }`;
@@ -29,12 +25,8 @@ const Timer = ({
     }
     return () => clearInterval(interval);
   }, [timerStatus, seconds]);
-  return (
-    <div className="timer">
-      {timeConverter()}
-      <button onClick={startTimer}>Start</button>
-    </div>
-  );
+
+  return <div className="timer">{timeConverter()}</div>;
 };
 
 export default Timer;
